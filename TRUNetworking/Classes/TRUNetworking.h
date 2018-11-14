85c076b509930b1aa09810703821f97e036eb22c
kevinxuelei <kevinxuelei@163.com>
2018-11-14 18:30:28 +0800

    init project

---
+++ TRUNetworking/Classes/TRUNetworking.h
@@ -0,0 +1,16 @@
+//
+//  TRUNetworking.h
+//  FBSnapshotTestCase
+//
+//  Created by 董学雷 on 2018/11/14.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TRUNetworking : NSObject
++(void)printSomething;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRUNetworking/Classes/TRUNetworking.m
@@ -0,0 +1,14 @@
+//
+//  TRUNetworking.m
+//  FBSnapshotTestCase
+//
+//  Created by 董学雷 on 2018/11/14.
+//
+
+#import "TRUNetworking.h"
+
+@implementation TRUNetworking
++(void)printSomething{
+    NSLog(@"123");
+}
+@end
